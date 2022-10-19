b6378f0e982d0b25ca52c082e3e8812b5f24d22a
Elena Nalbantova <48248363+NalbantovaElena@users.noreply.github.com>
2022-10-19 11:09:51 +0300

    Update Expenses.js
    Dinamic render ExpenseItem with built in method in JavaScript for array - Map

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -6,6 +6,7 @@ import ExpensesFilter from './ExpensesFilter';
 import './Expenses.css';
 
 const Expenses = (props) => {
+  
   const [filteredYear, setFilteredYear] = useState('2020');
 
   const filterChangeHandler = selectedYear => {
@@ -16,27 +17,16 @@ const Expenses = (props) => {
     <div>
       <Card className='expenses'>
         <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-        <ExpenseItem
-          title={props.items[0].title}
-          amount={props.items[0].amount}
-          date={props.items[0].date}
-        />
-        <ExpenseItem
-          title={props.items[1].title}
-          amount={props.items[1].amount}
-          date={props.items[1].date}
-        />
-        <ExpenseItem
-          title={props.items[2].title}
-          amount={props.items[2].amount}
-          date={props.items[2].date}
-        />
-        <ExpenseItem
-          title={props.items[3].title}
-          amount={props.items[3].amount}
-          date={props.items[3].date}
-        />
-      </Card>
+      {/* Имаме достъп до props.items */}
+       {props.items.map((expenses)=>(
+      <ExpenseItem
+      title={expenses.title}
+      amount={expenses.amount}
+      date={expenses.date}
+       />
+
+       ))}
+    </Card>
     </div>
   );
 };
