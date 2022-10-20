260bd54453f920546ce0f407c95aeb07eb860e1e
Elena Nalbantova <48248363+NalbantovaElena@users.noreply.github.com>
2022-10-20 10:51:54 +0300

    Update Expenses.js
    Add filter as value select element and year from ExpenseItem

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -6,21 +6,32 @@ import ExpensesFilter from './ExpensesFilter';
 import './Expenses.css';
 
 const Expenses = (props) => {
-  //console.log(props.items)
+  console.log(props.items)
   const [filteredYear, setFilteredYear] = useState('2020');
 
   const filterChangeHandler = selectedYear => {
     setFilteredYear(selectedYear);
   };
+   console.log(filteredYear)
+ //създаваме нова променлива в която чрез вградената функционалност 
+ //на JS - filter - филтрираме като датата(годината) от ExpenseItem
+ //трябва да  е равна на стойността от годината която е селектирана
+   const filteredExpenses = props.items.filter(expense=>{
+    return expense.date.getFullYear().toString()===filteredYear
+   })
 
   return (
     <div>
       <Card className='expenses'>
-        <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-      {/* Имаме достъп до props.items  и използваме вградения метод за обхождане на масив в JS - map*/}
-       {props.items.map((expenses)=>(
+        <ExpensesFilter 
+        selected={filteredYear} 
+        onChangeFilter={filterChangeHandler} />
+      {/* Имаме достъп до props.items  и използваме вградения
+       метод за обхождане на масив в JS - map*/}
+       
+       {filteredExpenses.map((expenses)=>(
         //тук след expenses може да се зададе index, (expenses, index)
-
+        
       <ExpenseItem
       //добавяме ключ във всеки елемент, за да е уникален
       //В react това е концепция създадена , за да се рендерира 
